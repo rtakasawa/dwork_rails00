c5a6f11a15d37af2b2e0844c108622608e05d45c
ryosuke takasawa <ten.r0619@gmail.com>
2020-02-29 08:34:02 +0900

    編集機能を作成した

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -8,9 +8,20 @@ class NotesController < ApplicationController
   def create
     @note = Note.new(note_params)
     if @note.save
-    　redirect_to note_path, notice: "投稿しました"
+      redirect_to notes_path, notice: "投稿しました"
     else
       render :new
+    end
+  end
+  def edit
+    @note = Note.find(params[:id])
+  end
+  def update
+    @note = Note.find(params[:id])
+    if @note.update(note_params)
+      redirect_to notes_path, notice: "投稿内容を編集しました！"
+    else
+      render :edit
     end
   end
   private
