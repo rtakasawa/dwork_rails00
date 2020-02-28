1d56a0f1d00ff533f5b14b4ae1cb1dc8f1d9e215
ryosuke takasawa <ten.r0619@gmail.com>
2020-02-28 15:11:58 +0900

    投稿機能を作成した。DBへの登録も確認済み

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,15 @@
 class NotesController < ApplicationController
   def index
   end
+  def new
+    @note = Note.new
+  end
+  def create
+    Note.create(note_params)
+    redirect_to new_note_path
+  end
+  private
+  def note_params
+    params.require(:note).permit(:content)
+  end
 end
